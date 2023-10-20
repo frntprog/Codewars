0b18a6ab79ef4ffc8b2da0bcc5fecb3fcfa7a438
frntprog <frntprog@gmail.com>
2023-10-20 21:43:16 +0300

    doen 6 kyu

--- 6kata/Number_number_wait_LETTER.js
+++ 6kata/Number_number_wait_LETTER.js
@@ -1,4 +1,6 @@
 function doMath(string) {
+  const operations = ["+", "-", "*", "/"];
+  let res = "";
   let arr = string
     .split(" ")
     .map((el) => {
@@ -10,14 +12,46 @@ function doMath(string) {
         letter: el.match(/[a-zA-Z]/)[0],
       };
     })
-    .sort((a, b) => (a.letter > b.letter ? 1 : -1));
+    .sort((a, b) => {
+      if (a.letter !== b.letter) {
+        return a.letter > b.letter ? 1 : -1;
+      }
+    });
 
-  return;
+  let oper = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (oper > 3) oper = 0;
+    res += arr[i].num;
+    if (i + 1 === arr.length) break;
+    res += operations[oper];
+    oper++;
+  }
+
+  const elements = res
+    .split(/(\+|\-|\*|\/)/)
+    .filter((element) => element.trim() !== "");
+
+  let result = parseFloat(elements[0]);
+
+  for (let i = 1; i < elements.length; i += 2) {
+    const operator = elements[i];
+    const number = parseFloat(elements[i + 1]);
+
+    if (operator === "+") {
+      result += number;
+    } else if (operator === "-") {
+      result -= number;
+    } else if (operator === "*") {
+      result *= number;
+    } else if (operator === "/") {
+      result /= number;
+    }
+  }
+
+  return Math.round(result);
 }
 
-const str = "24z6 1x23 y369 89a 900b";
+const str = "24z6 1z23 y369 89z 900b";
 
 console.log(doMath(str));
-
-// var str2 = "24z6";
-// var matchResult = str2.match(/[0-9]+/);
