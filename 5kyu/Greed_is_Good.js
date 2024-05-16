5829f6091e31f9ab7a842326cb9d1d0dc1a23d16
frntprog <frntprog@gmail.com>
2024-05-16 16:50:57 +0300

    5kyu

---
+++ 5kyu/Greed_is_Good.js
@@ -0,0 +1,48 @@
+function score(dice) {
+  const points = {
+    3: 1000,
+    6: 200,
+    9: 300,
+    12: 400,
+    15: 500,
+    18: 600,
+    1: 100,
+    5: 50,
+  };
+
+  const hash = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+  };
+
+  let score = 0;
+
+  for (const iterator of dice) {
+    hash[iterator] += 1;
+
+    if (hash[iterator] == 3) {
+      score += points[iterator * 3];
+      hash[iterator] = 0;
+    }
+  }
+
+  console.log(hash);
+
+  for (const iterator of dice) {
+    if (
+      (iterator == 1 && hash[iterator] > 0) ||
+      (iterator == 5 && hash[iterator] > 0)
+    ) {
+      score += points[iterator];
+      hash[iterator] -= 1;
+    }
+  }
+
+  return score;
+}
+
+console.log(score([1, 1, 1, 1, 2]));
